a0d48779864f827f9e948e4e1225fd3892ea2d8a
Sankha Narayan Guria <sankha93@gmail.com>
2019-12-08 16:22:41 -0500

    use spici

---
+++ cluster.py
@@ -0,0 +1,36 @@
+import csv
+import logging
+
+history = {}
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+
+with open('cluster.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter='\t')
+    node_id = 0
+    for row in csv_reader:
+        for item in row:
+            history[item] = node_id
+        node_id += 1
+
+logging.info("Read clusters")
+
+with open('data.tsv') as src:
+    reader = csv.reader(src, delimiter='\t')
+    with open('output.tsv', 'w') as dst:
+        writer = csv.writer(dst, delimiter='\t')
+        for row in reader:
+            lhs = row[0]
+            rhs = row[1]
+            if lhs not in history:
+                history[lhs] = node_id
+                node_id += 1
+            if rhs not in history:
+                history[rhs] = node_id
+                node_id += 1
+
+            if history[lhs] == history[rhs]:
+                continue
+            else:
+                writer.writerow([history[lhs], history[rhs]])
+
+logging.info("wrote reduced graph")
